38e7cfa4fed7b390119ce9d1b29521f05c969a85
Oluwatobi <amure387@gmail.com>
2023-05-01 20:53:37 +0000

    fix: make_response

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''State Rest API'''
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models.state import State
 from models import storage
 from api.v1.views import app_views
